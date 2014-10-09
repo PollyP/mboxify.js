16be77180e7c19b37588187b149a15f352c7a4b8
pollyp <pollyp@gmail.com>
2014-10-08 19:39:43 -0700

    fix comment typo

--- mboxify.js
+++ mboxify.js
@@ -4,8 +4,6 @@
 // copyright (c) 2014 Polly Powledge
 // The MIT License (MIT)
 //
-// Copyright (c) 2014 PollyP
-//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
